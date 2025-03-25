1e396869a33f37767e3c0fd1d487f2be24c51ec8
RuralWaste <ruralwaste18man@gmail.com>
2025-03-25 21:08:35 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -21,14 +21,19 @@ window.addEventListener('load', function() {
 // Fly in animation
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        if(entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-      }
+        if(phoneScreenQuery) {
+            entry.target.classList.remove('hidden');
+        }
+        else {
+            if(entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+          }
+        }
     });
 });
-
+const phoneScreenQuery = window.matchMedia('(max-width: 680px)');
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
